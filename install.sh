6215c1a06e5fc1bda7b3daf0d07a27afc711e466
Cheng Jiang <49266685+covertpluto3502@users.noreply.github.com>
2020-10-01 21:08:24 +0100

    Small changes

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+echo Installing...(0%)
+sleep 5
+echo Installing...(45%)
+sleep 4
 sudo pip install flask
 sudo apt-get install fswebcam
 mkdir webcam
+echo Installing...(84%)
+sudo pip install gpiozero
+echo Done(100%)
