224914788b3919c3ff4214e1cfdaa59736fbbec9
Alex Jiang <49266685+covertpluto3502@users.noreply.github.com>
2020-10-11 14:39:27 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,2 @@
-echo Starting server...
-echo Server up at control.local:5000
-sudo hostname control
-export FLASK_APP=control_pi_4.py
+export FLASK_APP=control_pi_four.py
 sudo flask run --host=0.0.0.0
